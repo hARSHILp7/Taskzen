7555f4b1e594488f0cc4124a29803928a0506bee
Harshil Prajapati <prajapatiharshil3107@gmail.com>
2026-02-28 10:39:03 -0500

    Message display functionality completed

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,1 +1,9 @@
-console.log('Todo App loaded');
+const submitBtn = document.getElementById('submit');
+const userInput = document.getElementById('message');
+const display = document.getElementById('display');
+
+submitBtn.addEventListener('click', () => {
+    display.textContent = userInput.value;
+    userInput.style.display = 'none';
+    submitBtn.style.display = 'none';
+});
