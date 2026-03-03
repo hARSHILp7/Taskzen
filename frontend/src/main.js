a22e724c855442752c924d2e4640b818ff9971b5
Harshil Prajapati <prajapatiharshil3107@gmail.com>
2026-03-03 18:12:41 -0500

    Vite and Tailwind CSS setup done

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,5 @@
+import './style.css'
+
 const submitBtn = document.getElementById('submit');
 const userInput = document.getElementById('message');
 const display = document.getElementById('display');
