6dd38f5dd1a998a0abd078fb3649980f02182fb5
Harshil Prajapati <prajapatiharshil3107@gmail.com>
2026-03-04 21:24:50 -0500

    Today's info and add task div done

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,1 +1,8 @@
 import './style.css'
+
+const add_task_input = document.getElementById('add-task-input')
+
+// Focus on the add task input after 3 seconds of page load
+setTimeout(() => {
+  add_task_input.focus();
+}, 3000);
